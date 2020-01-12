a2a4ba4977870e7b65236b3ed0b290aeb43a2886
Blackscure <Wekesabuyahi@gmail.com>
2020-01-12 16:55:07 +0300

    script file

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var firstName = "Read";
+//var firstName = "Read";
 
  
 
-var surName = "Burger";
+//var surName = "Burger";
 
-var s1000 = "one thousand";
+//var s1000 = "one thousand";
 
-console.log(firstName);
-console.log(surName);
-console.log(s1000);
+//console.log(firstName);
+//console.log(surName);
+//console.log(s1000);
+
+
+
+// FUNCTIONS
+
+function testExample() {
+    var greeting = "Hi! My name is Buyahi";
+    return greeting;
+}
+
+console.log(testExample());
